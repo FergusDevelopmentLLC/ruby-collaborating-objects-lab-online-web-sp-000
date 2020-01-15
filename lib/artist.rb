9cc2aea26ef842f75c1b6b03b10477c77093804c
Will Carter <will.carter@fergusllc.com>
2020-01-15 17:25:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,6 +12,7 @@ class Artist
   end
 
   def songs
+
     #Song.all.select {|song| song.artist.nil? == false && song.artist.name == self.name}
     Song.all.select {|song| song.artist == self}
   end
@@ -20,6 +21,7 @@ class Artist
     song.artist = self
   end
 
+<<<<<<< HEAD
   def self.find_or_create_by_name(name)
     matching_artists = self.all.select {|artist| artist.name == name}
     if (matching_artists.length == 0)
@@ -35,4 +37,6 @@ class Artist
     end
   end
 
+=======
+>>>>>>> 7710b1e56e1b0991866475d687efb958dbca05f4
 end
